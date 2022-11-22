7b9910d188ba629f8430722d78d364fa5947dc9b
Hermann Rasch <hermann.rasch93@gmail.com>
2022-11-22 10:38:31 +0100

    Revision & adjustments
    Typo corrections.
    Adjustment to variable schemas.

--- models.js
+++ models.js
@@ -1,5 +1,5 @@
 const {default:mongoose} = require('mongoose');
-let movieSchema = mongoose.Schema({
+let moviesSchema = mongoose.Schema({
     Title: {type: String, require: true},
     Description: {type: String, require:true},
     Genre: {
@@ -16,15 +16,15 @@ let movieSchema = mongoose.Schema({
     ImagePath: String,
     Feature: Boolean
 });
-let userSchema = mongoose.Schema({
+let usersSchema = mongoose.Schema({
     Username: {type:String, required:true},
     Password: {type:String, require:true},
     Email: {type:String, require:true},
     Birthday: Date,
     FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref:'Movie'}]
 });
-let Movie = mongoose.model('Movie', movieSchema);
-let User = mongoose.model('User', userSchema);
+let Movie = mongoose.model('Movie', moviesSchema);
+let User = mongoose.model('User', usersSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
