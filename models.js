b8e2f5c2c9f1c993dcf3e0216c85110a23badc42
Hermann Rasch <hermann.rasch93@gmail.com>
2022-11-23 14:51:29 +0100

    Typo corrections

--- models.js
+++ models.js
@@ -1,5 +1,5 @@
 const {default:mongoose} = require('mongoose');
-let moviesSchema = mongoose.Schema({
+let MoviesSchema = mongoose.Schema({
     Title: {type: String, require: true},
     Description: {type: String, require:true},
     Genre: {
@@ -16,15 +16,15 @@ let moviesSchema = mongoose.Schema({
     ImagePath: String,
     Feature: Boolean
 });
-let usersSchema = mongoose.Schema({
+let UsersSchema = mongoose.Schema({
     Username: {type:String, required:true},
     Password: {type:String, require:true},
     Email: {type:String, require:true},
     Birthday: Date,
     FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref:'Movie'}]
 });
-let Movie = mongoose.model('Movie', moviesSchema);
-let User = mongoose.model('User', usersSchema);
+let Movie = mongoose.model('Movie', MoviesSchema);
+let User = mongoose.model('User', UsersSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
