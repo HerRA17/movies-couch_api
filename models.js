daa94765a4d3ce3f06b15d8ee3d3968f6e88860b
Hermann Rasch <hermann.rasch93@gmail.com>
2022-11-28 21:01:11 +0100

    Adjustments and Updated File
    Addition of bcrypt.
    Inclution of hash Password.
    Added user Schema to hash password and validate it.

--- models.js
+++ models.js
@@ -1,5 +1,5 @@
 const {default:mongoose} = require('mongoose');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 let MoviesSchema = mongoose.Schema({
     Title: {type: String, require: true},
     Description: {type: String, require:true},
@@ -24,12 +24,12 @@ let UsersSchema = mongoose.Schema({
     Birthday: Date,
     FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref:'Movie'}]
 });
-// userSchema.statics.hashPassword = (password) => {
-//     return bcrypt.hashSync(password, 10);
-// };
-// userSchema.methods.validatePassword = function(password) {
-//     return bcrypt.compareSync(password, this.Password);
-// };
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+};
+userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password);
+};
 let Movie = mongoose.model('Movie', MoviesSchema);
 let User = mongoose.model('User', UsersSchema);
 
