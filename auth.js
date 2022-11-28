10f8a5ab2423c84f3195d4711d8f2add17d8e6d7
Hermann Rasch <hermann.rasch93@gmail.com>
2022-11-28 20:16:33 +0100

    Small adjustments to the file

--- auth.js
+++ auth.js
@@ -4,23 +4,28 @@ passport = require('passport');
 //local passport
 require('./passport');
  let generateJWTToken = (user) => {
-    return jwt.sign(user, jwtSecret, {
-    subject: user.Username,
-    expiresIn: '7d',
-    algorithm: 'HS256'
+    return jwt.sign(
+        user, jwtSecret, {
+        subject: user.Username,
+        expiresIn: '7d',
+        algorithm: 'HS256'
 });
  }
 //  POST login
 module.exports = (router) => {
     router.post('/login', (req, res) => {
-        passport.authenticate('local', {session: false}, (error, user, info) => {
+        passport.authenticate('local', {
+            session: false},
+            (error, user, info) => {
 if (error || !user) {
     return res.status(400).json({
         message: 'Something is wrong!',
         user: user
     });
 }
-req.login(user, {session: false}, (error) => {
+req.login(user, 
+    {session: false},
+     (error) => {
     if (error) {
         res.send(error);
     }
