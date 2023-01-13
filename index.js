bdcb90fded6f823700f7a305cf71698b6f05882d
Hermann Rasch <hermann.rasch93@gmail.com>
2023-01-13 10:55:26 +0100

    update file

--- index.js
+++ index.js
@@ -45,10 +45,10 @@ app.get("/movies", passport.authenticate('jwt', {session: false}),
     .then((movies) => {
       res.status(201).json(movies);
     })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: "+ err);
-    })
+    .catch((error) => {
+      console.error();
+      res.status(500).send("Error: " + error);
+    });
   });
 
 // get JSON movie info when looking for specific title
@@ -58,10 +58,10 @@ app.get("/movies/:title",  passport.authenticate('jwt', {session: false}),
     .then((movie) => {
       res.json(movie);
     })
-    .catch((err) => {
-    console.error(err);
-    res.status(404).send("Error: " + err);
-  });
+    .catch((error) => {
+      console.error();
+      res.status(500).send("Error: " + error);
+    });
 });
 
 // genre JSON genre info when looking for specific genre
@@ -71,9 +71,9 @@ app.get("/movies/genre/:name",  passport.authenticate('jwt', {session: false}),
     .then((movies) => {
       res.json(movies.Genre);
     }) 
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
+    .catch((error) => {
+      console.error();
+      res.status(500).send("Error: " + error);
     });
   });
 
@@ -96,10 +96,10 @@ app.get("/movies/director/:name/",  passport.authenticate('jwt', {session: false
     .then((movies) => {
       res.json(movies.Director);
     })
-    .catch((err) => {
+    .catch((error) => {
       console.error();
-      res.status(500).send("Error: " + err);
-    }) 
+      res.status(500).send("Error: " + error);
+    }); 
   });
 
 
@@ -109,9 +109,9 @@ app.get("/users",  passport.authenticate('jwt', {session: false}),
     Users.find().then((users) => {
         res.status(201).json(users);
     })
-    .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error: "+ err);
+    .catch((error) => {
+        console.error(error);
+        res.status(500).send("Error: "+ error);
     });
 }); 
 
@@ -121,8 +121,9 @@ app.get("/user/:Username",  passport.authenticate('jwt', {session: false}),
     Users.findOne({Username: req.params.Username })
     .then((user) => {res.json(user);
     })
-    .catch((error) => { console.error(error);
-        res.status(500).send("Error: "+ error);
+    .catch((error) => { 
+      console.error(error);
+      res.status(500).send("Error: "+ error);
     });
 });
 
@@ -193,10 +194,10 @@ app.post("/users/:Username/movies/:MovieID",  passport.authenticate('jwt', {sess
     Users.findOneAndUpdate({ usernam: req.params.Username}, {$push:{ FavoriteMovies: req.params.MovieID} 
     },
     {new:true}, //This line makes sure that the updated document is returned
-    (err, updatedUser) => {
-        if(err) {
-            console.error(err);
-            res.status(500).send("Error: "+ err);
+    (error, updatedUser) => {
+        if(error) {
+            console.error(error);
+            res.status(500).send("Error: "+ error);
         } else {
             res.json(updatedUser)
         }
@@ -209,10 +210,10 @@ app.delete("/users/:Username/movies/:MovieID",  passport.authenticate('jwt', {se
     Users.findOneAndUpdate({ usernam: req.params.Username},
       {$pull:{ FavoriteMovies: req.params.MovieID} },
     {new:true}, //This line makes sure that the updated document is returned
-    (err, updatedUser) => {
-        if(err) {
-            console.error(err);
-            res.status(500).send("Error: "+ err);
+    (error, updatedUser) => {
+        if(error) {
+            console.error(error);
+            res.status(500).send("Error: "+ error);
         } else {
             res.json(updatedUser)
         }
@@ -229,9 +230,9 @@ app.delete("/users/:Username",  passport.authenticate('jwt', {session: false}),
     } else { res.status(200).send(req.params.Username + "was deleted!");
    }
   })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error: "+ err);
+  .catch((error) => {
+    console.error(error);
+    res.status(500).send("Error: "+ error);
   });
 });
 
