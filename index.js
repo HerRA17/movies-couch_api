279a174297565e8e7f553e436ffa8846135e0355
Hermann <91563884+HerRA17@users.noreply.github.com>
2023-01-26 06:13:07 +0100

    temporary change

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const Directors = Models.Director;
 // mongoose.connect(
 //  process.env.CONNECTION_URI, 
 // { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect("mongodb+srv://hermann17:Chispa17@movies-couch-api.fyn8ikd.mongodb.net/?retryWrites=true&w=majority" , {useNewUrlParser: true, useUnifiedTopology: true}); 
+mongoose.connect("mongodb+srv://user:password@movies-couch-api.fyn8ikd.mongodb.net/?retryWrites=true&w=majority" , {useNewUrlParser: true, useUnifiedTopology: true}); 
 const app = express();
 app.use(bodyParser.json()); //Parse JSON bodies
 app.use(express.static("public")); //middleware for serving static files
