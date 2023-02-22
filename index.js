dfb9e0545f104683edb3799934c4a21b5ba2f7a2
Hermann <91563884+HerRA17@users.noreply.github.com>
2023-02-22 23:59:09 +0100

    temp-dev

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ let allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:1234",
   "http://localhost:4200",
+  "https://movies-couch-api-herra17.vercel.app/movies",
   "https://movies-couch-api.vercel.app/movies"
 ];
 // check if the domain where the request came from is allowed
