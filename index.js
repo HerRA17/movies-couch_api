563abd042613af71a49b91e8bdce984dd4925a61
Hermann <91563884+HerRA17@users.noreply.github.com>
2023-02-22 23:33:39 +0100

    temporary

--- index.js
+++ index.js
@@ -47,9 +47,9 @@ app.use(cors({
 const {check, validationResult } = require("express-validator");
 
 // Run passport file where strategies are implemented
-const passport = require ("passport");
-require("./passport");
-require("./auth")(app);
+// const passport = require ("passport");
+// require("./passport");
+// require("./auth")(app);
 
 // get requests- default text response
 app.get("/", (req, res) => {
@@ -57,7 +57,7 @@ app.get("/", (req, res) => {
 });
 
 // return JSON object when at /movies
-app.get("/movies",  passport.authenticate('jwt', {session: false}), 
+app.get("/movies",  //passport.authenticate('jwt', {session: false}), 
 (req, res) => {
     Movies.find()
     .then((movies) => {
@@ -70,7 +70,7 @@ app.get("/movies",  passport.authenticate('jwt', {session: false}),
   });
 
 // get JSON movie info when looking for specific title
-app.get("/movies/:title",   passport.authenticate('jwt', {session: false}),  
+app.get("/movies/:title",   //passport.authenticate('jwt', {session: false}),  
 (req, res) => {
   Movies.findOne({Title:  req.params.title})
     .then((movie) => {
@@ -83,7 +83,7 @@ app.get("/movies/:title",   passport.authenticate('jwt', {session: false}),
 });
 
 // genre JSON genre info when looking for specific genre
-app.get("/movies/genre/:name",  passport.authenticate('jwt', {session: false}),  
+app.get("/movies/genre/:name",  //passport.authenticate('jwt', {session: false}),  
 (req, res) => {
   console.log(req.params);
   Movies.findOne({"Genre.Name": req.params.name}) 
@@ -99,7 +99,7 @@ app.get("/movies/genre/:name",  passport.authenticate('jwt', {session: false}),
 
   
 // get info on Director when looking for specific Director
-app.get("/movies/director/:name/",  passport.authenticate('jwt', {session: false}),  
+app.get("/movies/director/:name/",  //passport.authenticate('jwt', {session: false}),  
 (req, res) => {
   Movies.findOne({"Director.Name": req.params.name }) 
     .then((movies) => {
@@ -112,7 +112,7 @@ app.get("/movies/director/:name/",  passport.authenticate('jwt', {session: false
   });
 
 // --GET one user
-app.get("/users/:Username",  passport.authenticate('jwt', {session: false}),  
+app.get("/users/:Username",  //passport.authenticate('jwt', {session: false}),  
 (req,res) => {
     Users.findOne({Username: req.params.Username })
     .then((user) => {res.json(user);
@@ -124,7 +124,7 @@ app.get("/users/:Username",  passport.authenticate('jwt', {session: false}),
 });
 
 // --FindOne & Update - allow users to update their user info
-app.put("/users/:Username",  passport.authenticate('jwt', {session: false}), 
+app.put("/users/:Username",  //passport.authenticate('jwt', {session: false}), 
 (req, res) => {
   if (req.user.Username !== req.params.Username)
   {
@@ -191,7 +191,7 @@ app.post("/users",
 });
 
 // --POST a movie to user Favorite Movies
-app.post("/users/:Username/movies/:MovieID",  passport.authenticate('jwt', {session: false}), 
+app.post("/users/:Username/movies/:MovieID",  //passport.authenticate('jwt', {session: false}), 
 (req,res) => {
     Users.findOneAndUpdate({ Username: req.params.Username}, {$push:{ FavoriteMovies: req.params.MovieID} 
     },
@@ -207,7 +207,7 @@ app.post("/users/:Username/movies/:MovieID",  passport.authenticate('jwt', {sess
 });
 
 // --DELETE remove a movie from Favorite Movies
-app.delete("/users/:Username/movies/:MovieID",  passport.authenticate('jwt', {session: false}),  
+app.delete("/users/:Username/movies/:MovieID",  //passport.authenticate('jwt', {session: false}),  
 (req,res) => {
     Users.findOneAndUpdate({ Username: req.params.Username},
       {$pull:{ FavoriteMovies: req.params.MovieID} },
@@ -223,7 +223,7 @@ app.delete("/users/:Username/movies/:MovieID",  passport.authenticate('jwt', {se
 });
 
 // --DELETE user by username- allow user to deregister
-app.delete("/users/:Username",  passport.authenticate('jwt', {session: false}), 
+app.delete("/users/:Username",  //passport.authenticate('jwt', {session: false}), 
 (req,res) => {
   if (req.user.Username !== req.params.Username)
   {
