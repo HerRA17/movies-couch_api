9fe44ea06057c641f25afc57317fc7a9a42727aa
Hermann <91563884+HerRA17@users.noreply.github.com>
2023-02-22 20:29:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,17 +28,14 @@ let allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:1234",
   "http://localhost:4200",
-  "https://movies-couch-api-git-main-herra17.vercel.app/",
-  "https://movies-couch-api-herra17.vercel.app/",
   "https://movies-couch-api.vercel.app/movies"
 ];
 // check if the domain where the request came from is allowed
 // app.use(cors());// dev-test
-app.use(
-  cors({
+app.use(cors({
     origin: (origin, callback) => {
       if(!origin) return callback(null, true);
-      if(allowedOrigins.indexOf(origin) === -1)) {
+      if(allowedOrigins.indexOf(origin) === -1)) {// If a specific origin isn’t found on the list of allowed origins
         let message =
         "The CORS policy for this application doesn't allow access from origin" + origin;
         return callback(new Error(message), false); 
