0713ce82485ab443a9963459e4f1964ce2718f84
Hermann <91563884+HerRA17@users.noreply.github.com>
2023-02-22 23:57:02 +0100

    temporary

--- index.js
+++ index.js
@@ -31,18 +31,18 @@ let allowedOrigins = [
   "https://movies-couch-api.vercel.app/movies"
 ];
 // check if the domain where the request came from is allowed
-// app.use(cors());// dev-test
-app.use(cors({
-    origin: (origin, callback) => {
-      if(!origin) return callback(null, true);
-      if(allowedOrigins.indexOfOrigin === -1) {// If a specific origin isn’t found on the list of allowed origins
-        let message =
-        "The CORS policy for this application doesn't allow access from origin" + origin;
-        return callback(new Error(message), false); 
-      }
-      return callback(null, true);
-    },
-  }));
+app.use(cors());// dev-test
+// app.use(cors({
+//     origin: (origin, callback) => {
+//       if(!origin) return callback(null, true);
+//       if(allowedOrigins.indexOfOrigin === -1) {// If a specific origin isn’t found on the list of allowed origins
+//         let message =
+//         "The CORS policy for this application doesn't allow access from origin" + origin;
+//         return callback(new Error(message), false); 
+//       }
+//       return callback(null, true);
+//     },
+//   }));
 // import <express-validator>- Middleware for validating methods on the backend
 const {check, validationResult } = require("express-validator");
 
