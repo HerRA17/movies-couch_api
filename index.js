736bf8a42d6fd1741cb698692f612c8866290927
Hermann <91563884+HerRA17@users.noreply.github.com>
2023-02-22 19:54:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(
   cors({
     origin: (origin, callback) => {
       if(!origin) return callback(null, true);
-      (allowedOrigins.indexOf(origin) === -1)) {
+      if(allowedOrigins.indexOf(origin) === -1)) {
         let message =
         "The CORS policy for this application doesn't allow access from origin" + origin;
         return callback(new Error(message), false); 
